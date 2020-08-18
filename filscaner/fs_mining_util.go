fb0eab1e095c1c443689d05a7bddf01a0e14787b
wen <308260887@qq.com>
2020-08-18 11:39:21 +0800

    modify fs_mining_util.go

--- filscaner/fs_mining_util.go
+++ filscaner/fs_mining_util.go
@@ -4,8 +4,9 @@ import (
 	errs "filscan_lotus/error"
 	"filscan_lotus/filscaner/force/factors"
 	"filscan_lotus/models"
-	"github.com/filecoin-project/lotus/chain/actors"
+	//"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/specs-actors/actors/builtin"
 )
 
 type MinerStateRecord struct {
@@ -40,12 +41,12 @@ func ParseActorMessage(message *types.Message) (*factors.ActorInfo, *factors.Met
 	}
 	actor, exist := factors.LookupByAddress(message.To)
 	if !exist {
-		if actor, exist = factors.Lookup(actors.StorageMinerCodeCid); !exist {
+		if actor, exist = factors.Lookup(builtin.StorageMinerActorCodeID); !exist {
 			return nil, nil, errs.ErrActorNotFound
 		}
 	}
 
-	method, exist := actor.LookupMethod(message.Method)
+	method, exist := actor.LookupMethod(uint64(message.Method))
 	if !exist {
 		return nil, nil, errs.ErrMethodNotFound
 	}
